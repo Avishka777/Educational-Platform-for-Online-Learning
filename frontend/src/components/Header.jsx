ebacdade95c6eaa942b1ac3c1e48762d890a5812
Avishka777 <a.rathnakumara777@gmail.com>
2024-04-24 21:46:53 +0530

    Complete Dark Mode Function

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,59 +1,129 @@
-import { Button, Navbar, TextInput } from 'flowbite-react';
-import { Link, useLocation } from 'react-router-dom';
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon } from 'react-icons/fa';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleTheme } from '../redux/theme/themeSlice';
 import logo from '../assets/logo.png';
+import { signoutSuccess } from '../redux/user/userSlice';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
-  
-    const path = useLocation().pathname;
-  
-    return (
+  const path = useLocation().pathname;
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
+  const { theme } = useSelector((state) => state.theme);
+  const [searchTerm, setSearchTerm] = useState('');
 
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  return (
     <Navbar className='border-b-2'>
-
-      <Navbar.Toggle />
-
-      <Navbar.Brand href="/">
-        <img src={logo} className="mr-3 h-9 sm:h-12" alt="Company Logo" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Knowledge.Net</span>
-      </Navbar.Brand>
-
-      <Navbar.Collapse>
-        <Navbar.Link active={path === "/"} as={'div'}>
-          <Link to='/'>Home</Link>
-        </Navbar.Link>
-        <Navbar.Link active={path === "/projects"} as={'div'}>
-          <Link to='/courses'>Courses</Link>
-        </Navbar.Link>
-        <Navbar.Link active={path === "/projects"} as={'div'}>
-          <Link to='/about'>About</Link>
-        </Navbar.Link>
-      </Navbar.Collapse>
-
-      <form>
-        <TextInput
-          type='text'
-          placeholdProductser='Search...'
-          rightIcon={AiOutlineSearch}
-          className='hidden lg:inline'
-        />
+      
+      <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white flex'>
+        <img src={logo} className="mr-3 h-12 sm:h-12" alt="Company Logo" />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white font-serif text-sky-700">Knowledge.Net</span>
+      </Link>
+      
+      <form onSubmit={handleSubmit}>
+        <TextInput 
+          type='text' 
+          placeholder='Search...' 
+          rightIcon={AiOutlineSearch} 
+          className='hidden lg:inline' 
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}/>
       </form>
-
-      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+      <Button className='w-12 h-10 hidden' color='gray' pill>
         <AiOutlineSearch />
       </Button>
 
-      <div className='flex gap-2 md:order-2 '>
-        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
-          <FaMoon />
+      <div className='flex gap-2 md:order-2'>
+        <Button
+          className='sm:inline rounded-lg'
+          color='gray'
+          pill
+          onClick={() => dispatch(toggleTheme())}
+        >
+          {theme === 'light' ? <FaMoon /> : <FaSun />}
         </Button>
-        <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue'>Sign In</Button>
-        </Link>
-
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt='user' img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className='block text-sm'>@{currentUser.userName}</span>
+              <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to='/sign-in'>
+            <Button 
+              gradientDuoTone='purpleToBlue' 
+              outline
+              className='w-20 h-10 sm:inline rounded-lg'
+            >
+              <p className='text-xs'>Sign In</p>
+            </Button>
+          </Link>
+        )}
+        <Navbar.Toggle />
       </div>
-
+      <Navbar.Collapse>
+        <Navbar.Link active={path === '/'} as={'div'} style={{ color: path === "/" ? "#8043d6" : "#1f96c1" }}>
+          <Link to='/'>Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/products'} as={'div'} style={{ color: path === "/products" ? "#8043d6" : "#1f96c1" }} >
+          <Link to='/products'>Products</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/about'} as={'div'} style={{ color: path === "/about" ? "#8043d6" : "#1f96c1" }}>
+          <Link to='/about'>About Us</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/contact'} as={'div'} style={{ color: path === "/contact" ? "#8043d6" : "#1f96c1" }}>
+          <Link to='/contact'>Contact Us</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
