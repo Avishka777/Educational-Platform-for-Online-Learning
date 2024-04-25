6c8136cacaaeef9a8d449b5cc2d6e4b58d9479e5
Avishka777 <a.rathnakumara777@gmail.com>
2024-04-25 17:14:19 +0530

    Design Sponsors Section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import HomeSlides from '../components/HomeBanners'
 import CourseCard from '../components/CourseCard'
+import Sponsors from '../components/Sponsors'
 
 export default function Home() {
   return (
     <div className='sm:px-36 py-10 px-10'>
       <HomeSlides/>
+      
       <div className='rounded-lg mt-10'>
           <h1 className='text-2xl sm:text-4xl font-serif text-center mb-5'>Recent Courses</h1>
           <hr className='my-1 sm:my-2 border-2 border-gray-500 font-bold' />
       </div>
       <CourseCard/>
+      
+      <div className='rounded-lg mt-10'>
+          <h1 className='text-2xl sm:text-4xl font-serif text-center mb-5'>Our Sponsors</h1>
+          <hr className='my-1 sm:my-2 border-2 border-gray-500 font-bold' />
+      </div>
+      <Sponsors/>
     </div>
   )
 }
