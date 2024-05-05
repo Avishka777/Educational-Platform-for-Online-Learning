2a4c58f7df2d48bb2c23eb7ea699f0ef07731a27
DhananjayaLakshan <abey.dhananjaya@gmail.com>
2024-05-04 21:53:14 -0700

    add model

---
+++ backend/course-service/models/post.model.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+const postSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: String,
+            // require: true
+        },
+        content: {
+            type: String,
+            require: true
+        },
+        title: {
+            type: String,
+            unique: true
+        },
+        decription: {
+            type: String,
+            require: true
+        },
+        image: {
+            type: String,
+            default: 'https://www.contentviewspro.com/wp-content/uploads/2017/07/default_image.png'
+        },
+        video: {
+            type: String,
+            default: 'https://videopromotion.club/assets/images/default-video-thumbnail.jpg'
+        },
+        category: {
+            type: String,
+            default: 'uncategorized',
+        },
+        slug: {
+            type: String,
+            require: true,
+            unique: true
+        },
+
+
+
+    }, { timestamps: true }
+)
+
+const Post = mongoose.model('Post', postSchema)
+export default Post
