027ec11b78e5345cb616276780da5dd4a46f988e
dimesha <dimesha99kanchani@gmail.com>
2024-05-10 11:50:02 +0530

    complete gateway

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const proxy = require("express-http-proxy");
-
 const app = express();
-
 app.use(cors());
 app.use(express.json());
+app.use("/authservice", proxy("http://localhost:5001"));
+app.use("/courseservice", proxy("http://localhost:5002"));
+app.use("/", proxy("http://localhost:5003")); // payments
 
-app.use("/auth-service", proxy("http://localhost:4001"));
-app.use("/course-service", proxy("http://localhost:4002"));
-app.use("/", proxy("http://localhost:4003")); // payments
-
-app.listen(8000, () => {
-  console.log("Gateway is Listening to Port 4000");
+app.listen(5000, () => {
+  console.log("Gateway is Listening to Port 5000");
 });
